de953ae90c4f8b2430429b1cdf391c7d712afc31
Emanuele Disco <emawind84@gmail.com>
2022-05-13 11:32:31 +0900

    - added up and down key handler to selector page UP key go to cards compendium DOWN key go to cards custom settings

--- makepk3.bat
+++ makepk3.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\7-Zip\7z.exe" a -tzip -mx=0 ../CorruptionCards-v3.2.pk3 * -r -x!makepk3.bat -xr!.git -x!*.pk3
+"C:\Program Files\7-Zip\7z.exe" a -tzip -mx=0 ../CorruptionCards-v3.4.pk3 * -r -x!makepk3.bat -xr!.git -x!*.pk3
